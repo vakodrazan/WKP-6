743d46a9e530299487e227944c27531fcdf3b359
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-08-10 07:54:03 +0300

    Remove the testing that I did and rename a variable

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ const foods = [
 
 const sumbmitOrder = document.querySelector(".sumbmitOrder");
 const article = document.querySelector("article");
-const spicy = document.querySelector("input[name=spicy]");
+const spicyFood = document.querySelector("#spicy");
 
 const foodOrder = () => {
     foods.forEach(foodList => {
@@ -56,13 +56,10 @@ const foodOrder = () => {
 // Filter the list when the check box is checked
 // Return only the value that has types of food checked
 
+// Check if the food is spicy
 const handleSpicyCheckbox = () => {
-	if (true/*spicy.checked*/) {
-		//article.removeChild(article.childNodes[5]);
-		for (let i=0; i<article.getElementsByTagName('*').length-5;i++) {
-			article.removeChild(article.childNodes[i+5]);
-		}
-		console.log("tree");
+	if (spicyFood.checked /*true*/) {
+		console.log("Checked");
 		const filterSpicyFood = foods.filter(food => food.spicy);
 		filterSpicyFood.map(food => {
 			const myHtml = `
@@ -88,8 +85,8 @@ const handleSpicyCheckbox = () => {
 // 	console.log("Ordered food");
 // }
 
-//spicy.addEventListener('change', handleSpicyCheckbox);
+spicyFood.addEventListener('change', handleSpicyCheckbox);
 //window.addEventListener("click", handleSpicyCheckbox);
 // sumbmitOrder.addEventListener("click", handleSumbmitOrder);
 foodOrder();
-handleSpicyCheckbox();
+// handleSpicyCheckbox();
